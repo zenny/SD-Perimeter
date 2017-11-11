e92b9f404c4b031720bb75b94dbee28241a11d3e
foxxyben <foxxyben@gmail.com>
2017-11-11 18:12:12 -0500

    Added configuration file writing

--- broker/brokerInstall.sh
+++ broker/brokerInstall.sh
@@ -1,12 +1,113 @@
 #!/bin/bash
 
 ####Information Gathering
-read -sp "Choose a password for your database root user [Default: rootdbpass]" rootDBpass
-rootDBpass=${rootDBpass:-rootdbpass}
-echo ""
-read -sp "Choose a password for your OpenVPN database user [Default: ovpnpass]" ovpnDBpass
-ovpnDBpass=${ovpnDBpass:-ovpnpass}
-echo ""
+function infoGather {
+  OPENVPN_DIR=/etc/openvpn
+  DB_CONFIG=$OPENVPN_DIR/scripts/config.sh
+  ##Check for pre-existing configuration and create if it does not exist
+  if [ ! -e "$OPENVPN_DIR/scripts" ]
+    mkdir -p $OPENVPN_DIR/scripts
+  fi
+  if [ ! -e "$DB_CONFIG" ]; then
+    OPENVPN_CLIENT_FOLDER=$OPENVPN_DIR/client
+    OUTPUT_DIR=$OPENVPN_DIR/client-configs/files
+    BASE_CONFIG=$OPENVPN_DIR/client-configs/base.conf
+    BASE_WIN_FILES=$OPENVPN_DIR/client-configs/winfiles
+    OPENVPN_CLIENT_BASE=$OPENVPN_CLIENT_FOLDER/sdp-base
+    touch $DB_CONFIG
+    chmod +x $DB_CONFIG
+    mkdir -p $OPENVPN_CLIENT_FOLDER
+    mkdir -p $OUTPUT_DIR
+    mkdir -p $BASE_WIN_FILES
+    mkdir -p $OPENVPN_CLIENT_BASE
+    ##Write out initial config script
+    echo "#!/bin/bash" >> $DB_CONFIG
+    echo "####Directories" >> $DB_CONFIG
+    echo "OPENVPN_DIR=/etc/openvpn" >> $DB_CONFIG
+    echo "DB_CONFIG=\$OPENVPN_DIR/scripts/config.sh" >> $DB_CONFIG
+    echo "OPENVPN_CLIENT_FOLDER=\$OPENVPN_DIR/client" >> $DB_CONFIG
+    echo "OUTPUT_DIR=\$OPENVPN_DIR/client-configs/files" >> $DB_CONFIG
+    echo "BASE_CONFIG=\$OPENVPN_DIR/client-configs/base.conf" >> $DB_CONFIG
+    echo "BASE_WIN_FILES=\$OPENVPN_DIR/client-configs/winfiles" >> $DB_CONFIG
+    echo "OPENVPN_CLIENT_BASE=\$OPENVPN_CLIENT_FOLDER/sdp-base" >> $DB_CONFIG
+    echo "" > $DB_CONFIG
+  else
+    . $DB_CONFIG
+  fi  
+
+  ##Prompt user for information if not already gathered
+  if [ -n "$KEY_EMAIL" ]; then
+    read -p "Choose your Admin email [Default: admin@mail.com]" KEY_EMAIL
+    KEY_EMAIL=${KEY_EMAIL:-admin@mail.com}
+    echo "####Easy-RSA variables" >> $DB_CONFIG
+    echo "KEY_EMAIL=$KEY_EMAIL" >> $DB_CONFIG
+    echo ""
+  fi
+  if [ -n "$EASY_RSA_DIR" ]; then
+    read -p "Choose your EasyRSA installation Directory [Default: /etc/openvpn/easy-rsa]" OPENVPN_RSA_DIR
+    OPENVPN_RSA_DIR=${OPENVPN_RSA_DIR:-/etc/openvpn/easy-rsa}
+    OPENVPN_KEYS=$OPENVPN_RSA_DIR/keys
+    echo "OPENVPN_RSA_DIR=$OPENVPN_RSA_DIR" >> $DB_CONFIG
+    echo "OPENVPN_KEYS=\$OPENVPN_RSA_DIR/keys" >> $DB_CONFIG
+    echo ""
+  fi
+  if [ -n "$KEY_NAME" ]; then
+    read -p "Choose a name for your Certificate Authority [Default: MyCA]" KEY_NAME
+    KEY_NAME=${KEY_NAME:-MyCA}
+    echo "KEY_NAME=$KEY_NAME" >> $DB_CONFIG
+    echo ""
+  fi
+  if [ -n "$KEY_COUNTRY" ]; then
+    read -p "Choose the country for your CA [Default: US]" KEY_COUNTRY
+    KEY_COUNTRY=${KEY_COUNTRY:-US}
+    echo "KEY_COUNTRY=$KEY_COUNTRY" >> $DB_CONFIG
+    echo ""
+  fi
+  if [ -n "$KEY_PROVINCE" ]; then
+    read -p "Choose the state/province for your CA [Default: NA]" KEY_PROVINCE
+    KEY_PROVINCE=${KEY_PROVINCE:-NA}
+    echo "KEY_PROVINCE=$KEY_PROVINCE" >> $DB_CONFIG
+    echo ""
+  fi
+  if [ -n "$KEY_CITY" ]; then
+    read -p "Choose the city for your CA [Default: None]" KEY_CITY
+    KEY_CITY=${KEY_CITY:-None}
+    echo "KEY_CITY=$KEY_CITY" >> $DB_CONFIG
+    echo ""
+  fi
+  if [ -n "$KEY_ORG" ]; then
+    read -p "Choose the Orgonization for your CA [Default: None]" KEY_ORG
+    KEY_ORG=${KEY_ORG:-None}
+    echo "KEY_ORG=$KEY_ORG" >> $DB_CONFIG
+    echo ""
+  fi
+  if [ -n "$KEY_OU" ]; then
+    read -p "Choose the Organizational Unit for your CA [Default: None]" KEY_OU
+    KEY_OU=${KEY_OU:-None}
+    echo "KEY_OU=$KEY_OU" >> $DB_CONFIG
+    echo ""
+  fi
+  if [ -n "$rootDBpass" ]; then
+    read -sp "Choose a password for your database root user [Default: rootdbpass]" rootDBpass
+    rootDBpass=${rootDBpass:-rootdbpass}
+    echo ""
+  fi
+  if [ -n "$PASS" ]; then
+    read -sp "Choose a password for your OpenVPN database user [Default: ovpnpass]" PASS
+    PASS=${PASS:-ovpnpass}
+    USER='openvpn'
+    DB='openvpn'
+    HOST='127.0.0.1'
+    PORT='3306'
+    echo "####Database Setting" >> DB_CONFIG
+    echo "HOST=$HOST" >> $DB_CONFIG
+    echo "PORT=$PORT" >> $DB_CONFIG
+    echo "USER=$USER" >> $DB_CONFIG
+    echo "PASS=$PASS" >> $DB_CONFIG
+    echo "DB=$DB" >> $DB_CONFIG
+    echo ""
+  fi
+}
 
 ####Install required Packages
 function installPackages {
@@ -41,16 +142,17 @@ function configureDatabase {
   ## Special thanks: https://sysadmin.compxtreme.ro/how-to-install-a-openvpn-system-based-on-userpassword-authentication-with-mysql-day-control-libpam-mysql/
   mysql -u root -p$rootDBpass < mariadbconf/openvpn.sql
   ## Create OpenVPN database user
-  mysql -u root -p$rootDBpass -e "CREATE USER openvpn@'%' IDENTIFIED BY '${ovpnDBpass}'"
-  mysql -u root -p$rootDBpass -e "GRANT ALL PRIVILEGES ON openvpn.* TO 'openvpn'@'%'"
+  mysql -u root -p$rootDBpass -e "CREATE USER $USER@'%' IDENTIFIED BY '${PASS}'"
+  mysql -u root -p$rootDBpass -e "GRANT ALL PRIVILEGES ON $DB.* TO '$USER'@'%'"
   mysql -u root -p$rootDBpass -e "FLUSH PRIVILEGES"
   ## Put default user file in place
   cp mariadbconf/50-client.cnf /etc/mysql/mariadb.conf.d/
-  sed -i 's/password\=.*/password\=$ovpnDBpass/' /etc/mysql/mariadb.conf.d/50-client.cnf
+  sed -i 's/user\=.*/user\=$USER/' /etc/mysql/mariadb.conf.d/50-client.cnf
+  sed -i 's/password\=.*/password\=$PASS/' /etc/mysql/mariadb.conf.d/50-client.cnf
   ## Restart database
   service mysql restart
 }
 
-
+infoGather
 installPackages
 configureDatabase
