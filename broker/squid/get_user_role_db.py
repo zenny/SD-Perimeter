b0353782ac0b4616dcb6288c973a068d173619a8
Ben Fox <ben@ifoxxy.net>
2018-01-21 05:22:20 +0000

    More fixes to squid python helper. Should work much better now.

--- broker/squid/get_user_role_db.py
+++ broker/squid/get_user_role_db.py
@@ -1,40 +1,49 @@
 #!/usr/bin/python3
 
 import MySQLdb
+import sys
 
 HOST = "127.0.0.1"
 PORT = 3306
 USER = "sdpuser"
-PASS = "dvuiim69"
+PASS = "sdpdbpass"
 DB = "sdpdb"
 
-try:
-    id, srchost, resource = input("").split()
-except EOFError:
-    exit()
+while True:
+    line = sys.stdin.readline().strip()
+    arr = line.split()
+    id = arr[0]
+    srchost = arr[1]
+    resource = arr[2]
 
-try:
     db = MySQLdb.connect(host=(HOST),
         port=(PORT),
         user=(USER),
         passwd=(PASS),
         db=(DB))
-    cursor = db.cursor()
 
-    cursor.execute("""select user_id from squid_user_helper where log_remote_ip='%s'""" % (srchost) )
-    username = cursor.fetchone()
+    try:
+        cursor = db.cursor()
+        cursor.execute("""select user_id from squid_user_helper where log_remote_ip='%s'""" % (srchost) )
+        username = cursor.fetchone()
 
-    if resource == 'all_users':
-        permitted = [1]
-    else:
-        cursor.execute("""select count(*) from squid_rules_helper r, squid_group_helper u where r.resource_name='%s' and u.user='%s' and u.ugroup = r.ugroup_name""" % (resource, username[0]) )
-        permitted = cursor.fetchone()
+        if resource == 'all_users':
+            permitted = [1]
+        else:
+            cursor.execute("""select count(*) from squid_rules_helper r, squid_group_helper u where r.resource_name='%s' and u.user='%s' and u.ugroup = r.ugroup_name""" % (resource, username[0]) )
+            permitted = cursor.fetchone()
 
-    if permitted[0] > 0:
-        print(id + " OK user=" + username[0])
-    else:
-        print(id + " ERR user=" + username[0])
-except TypeError:
-    print(id + " ERR user=")
+        if permitted[0] > 0:
+            #print(id + " OK user=" + username[0])
+            sys.stdout.write(id + " OK user=" + username[0] + "\n")
+        else:
+            #print(id + " ERR user=" + username[0])
+            sys.stdout.write(id + " ERR user=" + username[0] + "\n")
+    except TypeError:
+        #print(id + " ERR user=")
+        sys.stdout.write(id + " ERR user=" + "\n")
 
-db.close()
+    sys.stdout.flush()
+    db.close()
+
+
