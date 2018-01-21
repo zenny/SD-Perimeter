1c99032b97c1bc82f351d936cd4cf9d516cc6999
Ben Fox <ben@ifoxxy.net>
2018-01-21 14:13:21 +0000

    cleanup

--- broker/brokerInstall.sh
+++ broker/brokerInstall.sh
@@ -608,6 +608,8 @@ function createManagement {
   chmod +x /opt/sdp/scripts/rebuild_squid_config.sh
   cp $DIR/scripts/manage_usergroups.sh /opt/sdp/scripts/
   chmod +x /opt/sdp/scripts/manage_usergroups.sh
+  cp $DIR/scripts/Manage_SDP.sh /opt/sdp/scripts/
+  chmod +x /opt/sdp/scripts/Manage_SDP.sh
 }
 
 ### Execute order
